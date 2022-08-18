d9bd31f8903c0e3cb93b2475b8b2c951069627b9
AndrickSilva <silva.andrick2001@gmail.com>
2022-08-18 12:10:29 +0530

    added menu

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// function myFunction() {
+//     var ll = document.getElementsByClassName('ll');
+//     var btn = document.getElementById('#myBtn');
+
+//     if (ll.style.display === "none") {
+//         btnText.innerHTML = "Read more";
+//     } else {
+//         ll.style.display = "none";
+//         btnText.innerHTML = "Read less";
+//         moreText.style.display = "inline";
+//     }
+// }
