be859aabc7fb573e8044353c9103d34a9c087012
AJ <silva.andrick2001@gamil.com>
2023-10-03 14:53:27 +0530

    new bg image and cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        lightGrey: "#edeef4",
+      }
+    },
   },
   plugins: [],
 }
